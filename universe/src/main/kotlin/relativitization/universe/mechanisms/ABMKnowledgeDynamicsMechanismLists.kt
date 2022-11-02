45b500f4d41ba4403e3e5c984ffee7607ce01df9
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-11-02 10:15:24 +0100

    Add ResetReward

--- universe/src/main/kotlin/relativitization/universe/mechanisms/ABMKnowledgeDynamicsMechanismLists.kt
+++ universe/src/main/kotlin/relativitization/universe/mechanisms/ABMKnowledgeDynamicsMechanismLists.kt
@@ -2,12 +2,14 @@ package relativitization.universe.mechanisms
 
 import relativitization.universe.mechanisms.knowledge.Innovation
 import relativitization.universe.mechanisms.knowledge.Production
+import relativitization.universe.mechanisms.knowledge.ResetReward
 import relativitization.universe.mechanisms.knowledge.SelectCooperator
 import relativitization.universe.mechanisms.knowledge.SyncCooperator
 
 object ABMKnowledgeDynamicsMechanismLists : MechanismLists() {
     override val regularMechanismList: List<Mechanism> = listOf(
         SyncCooperator,
+        ResetReward,
     )
 
     override val dilatedMechanismList: List<Mechanism> = listOf(
