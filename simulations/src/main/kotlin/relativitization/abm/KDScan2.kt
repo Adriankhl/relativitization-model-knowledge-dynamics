efc603abca72d9d55c052776c5248b6c9c9514b1
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-11-17 15:20:36 +0100

    Clean unnecessary variables for KDScan2

--- simulations/src/main/kotlin/relativitization/abm/KDScan2.kt
+++ simulations/src/main/kotlin/relativitization/abm/KDScan2.kt
@@ -11,53 +11,44 @@ import java.io.File
 fun main() {
     val dfList: MutableList<DataFrame<*>> = mutableListOf()
 
-    val preSelectionStrategyList: List<PreSelectionStrategy> =
-        PreSelectionStrategy.values().toList()
+    val randomSeedList: List<Long> = (100L..200L).toList()
 
-    val selectionStrategyList: List<SelectionStrategy> = SelectionStrategy.values().toList()
-
-    val randomSeedList: List<Long> = (100L..120L).toList()
-
-    for (preSelectionStrategy in preSelectionStrategyList) {
-        for (selectionStrategy in selectionStrategyList) {
-            for (randomSeed in randomSeedList) {
-                println("PreSelection: $preSelectionStrategy. Selection: $selectionStrategy. Random seed: $randomSeed")
-                dfList.add(
-                    knowledgeDynamicsSingleRun(
-                        printStep = false,
-                        numStep = 1000,
-                        randomSeed = randomSeed,
-                        xDim = 10,
-                        yDim = 10,
-                        zDim = 10,
-                        numPlayer = 102,
-                        speedOfLight = 200.0,
-                        sameLocation = 0,
-                        maxInitialCapability = 30,
-                        innovationHypothesisSize = 3,
-                        preferentialPower = 2.0,
-                        homophilyPower = 1.0,
-                        preSelectionTransitiveNum = 51,
-                        selectionPreferentialNum = 34,
-                        selectionHomophilyNum = 34,
-                        sequentialRun = 0,
-                        cooperationLength = 5,
-                        numPreSelectedFirm = 5,
-                        radicalThreshold = 6,
-                        incrementalThreshold = 8,
-                        maxCapability = 100,
-                        maxAbility = 10,
-                        maxExpertise = 20,
-                        numProduct = 20,
-                        maxProductQuality = 50,
-                        maxReward = 10,
-                        forgetProbability = 0.05,
-                        radicalInnovationProbability = 0.4,
-                        incrementalInnovationProbability = 0.1,
-                    )
-                )
-            }
-        }
+    for (randomSeed in randomSeedList) {
+        println("Random seed: $randomSeed")
+        dfList.add(
+            knowledgeDynamicsSingleRun(
+                printStep = false,
+                numStep = 1000,
+                randomSeed = randomSeed,
+                xDim = 10,
+                yDim = 10,
+                zDim = 10,
+                numPlayer = 102,
+                speedOfLight = 200.0,
+                sameLocation = 0,
+                maxInitialCapability = 30,
+                innovationHypothesisSize = 3,
+                preferentialPower = 2.0,
+                homophilyPower = 1.0,
+                preSelectionTransitiveNum = 51,
+                selectionPreferentialNum = 34,
+                selectionHomophilyNum = 34,
+                sequentialRun = 0,
+                cooperationLength = 5,
+                numPreSelectedFirm = 5,
+                radicalThreshold = 6,
+                incrementalThreshold = 8,
+                maxCapability = 100,
+                maxAbility = 10,
+                maxExpertise = 20,
+                numProduct = 20,
+                maxProductQuality = 50,
+                maxReward = 10,
+                forgetProbability = 0.05,
+                radicalInnovationProbability = 0.4,
+                incrementalInnovationProbability = 0.1,
+            )
+        )
     }
 
 
