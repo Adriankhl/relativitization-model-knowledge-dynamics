45953d331a551e39b14e40fc59232457725e5a0d
Adriankhl <dlshcbmuipmam@hotmail.com>
2023-01-16 23:53:21 +0100

    Improve run-all-simulations.sh

--- run-all-simulations.sh
+++ run-all-simulations.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-fileList=$(find ./simulations/src/main/kotlin/relativitization/abm ! -name "*Test*.kt" -type f | sed 's|^.*/||')
+fileList=$(find ./simulations/src/main/kotlin/relativitization/abm ! -name "*Test*.kt" -type f | sed 's|^.*/||' | sort)
 
 for fileName in $fileList; do
   className=${fileName::-3}
-  ./gradlew :simulations:run -PmainClass=relativitization.abm."$className"Kt -PprocessorCount=10 -PramPercentage=50
+  ./gradlew :simulations:run -PmainClass=relativitization.abm."$className"Kt -PprocessorCount=${1:-10} -PramPercentage=${2:-50}
 done
