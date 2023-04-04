64f5a159cab73cce8951cd4ee05e775ca2549f5f
Adriankhl <dlshcbmuipmam@hotmail.com>
2023-04-04 21:30:16 +0200

    Add run-simulations.sh

---
+++ run-simulations.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+fileList=(
+  "KDScan3.kt"
+  "KDScan4.kt"
+  "KDScan5.kt"
+  "KDScan6.kt"
+)
+
+for fileName in $fileList; do
+  className=${fileName::-3}
+  ./gradlew :simulations:run -PmainClass=relativitization.abm."$className"Kt -PprocessorCount=${1:-4} -PramPercentage=${2:-10}
+done
