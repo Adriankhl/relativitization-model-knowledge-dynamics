4123c1ef06cdacb5c198a98b02343550f604bb68
Adriankhl <dlshcbmuipmam@hotmail.com>
2023-04-04 23:21:20 +0200

    Fix run-simulations.sh script

--- run-simulations.sh
+++ run-simulations.sh
@@ -7,7 +7,7 @@ fileList=(
   "KDScan6.kt"
 )
 
-for fileName in $fileList; do
+for fileName in ${fileList[@]}; do
   className=${fileName::-3}
   ./gradlew :simulations:run -PmainClass=relativitization.abm."$className"Kt -PprocessorCount=${1:-4} -PramPercentage=${2:-10}
 done
