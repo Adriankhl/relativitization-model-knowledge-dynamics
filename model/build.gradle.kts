2e6852153812fc7dd1bb5e061a02a46dcc66a8ad
Adriankhl <dlshcbmuipmam@hotmail.com>
2023-04-26 21:16:02 +0200

    Add gradle files

---
+++ model/build.gradle.kts
@@ -0,0 +1,45 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.serialization")
+    id("org.jetbrains.dokka")
+    id("com.google.devtools.ksp")
+}
+
+dependencies {
+    ksp(libs.ksergen.ksp)
+
+    implementation(libs.relativitization.core)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlin.reflect)
+    implementation(libs.ksergen.annotations)
+    implementation(libs.log4j.api)
+    implementation(libs.log4j.core)
+}
+
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(libs.versions.jdkVersion.get()))
+    }
+}
+
+afterEvaluate {
+    tasks.named("kspTestKotlin") {
+        enabled = false
+    }
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions.jvmTarget = libs.versions.jvmTargetVersion.get()
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.release.set(libs.versions.jvmTargetVersion.get().toInt())
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
---
+++ simulations/build.gradle.kts
@@ -0,0 +1,71 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm")
+    application
+    id("org.jetbrains.dokka")
+}
+
+val processorCount: String = project.properties.getOrDefault(
+    "processorCount",
+    "NA"
+).toString()
+
+val ramPercentage: String = project.properties.getOrDefault(
+    "ramPercentage",
+    "NA"
+).toString()
+
+dependencies {
+    implementation(project(":model"))
+
+    implementation(libs.relativitization.core)
+    implementation(libs.kotlin.reflect)
+    implementation(libs.log4j.core)
+    implementation(libs.dataframe)
+}
+
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(libs.versions.jdkVersion.get()))
+    }
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions.jvmTarget = libs.versions.jvmTargetVersion.get()
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.release.set(libs.versions.jvmTargetVersion.get().toInt())
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
+
+tasks.register("showJVMArgs") {
+    doLast {
+        val totalNumProcessor: Int = Runtime.getRuntime().availableProcessors()
+        println("ActiveProcessorCount: $processorCount/$totalNumProcessor")
+        println("MaxRAMPercentage: $ramPercentage")
+    }
+}
+
+tasks.withType<JavaExec>().configureEach {
+    dependsOn("showJVMArgs")
+}
+
+application {
+    mainClass.set(project.properties["mainClass"].toString())
+    applicationDefaultJvmArgs = if (processorCount != "NA") {
+        listOf("-XX:ActiveProcessorCount=$processorCount")
+    } else {
+        listOf()
+    } + if (ramPercentage != "NA") {
+        listOf("-XX:MaxRAMPercentage=$ramPercentage")
+    } else {
+        listOf()
+    }
+}
