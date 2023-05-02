f08e6df1fa72e5a7d415d773dba539d3a1324882
Adriankhl <dlshcbmuipmam@hotmail.com>
2023-05-02 16:16:12 +0200

    build: remove unnecessary dependencies from libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.dokka)
-    alias(libs.plugins.ben.manes.versions)
     alias(libs.plugins.ksp)
 }
 
